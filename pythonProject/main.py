9248ef7d00106d426247a9763186a1e3c75f75b1
goldenapple545 <egorvikturovgmail.com git config --global user.email egorvikturovgmail.com>
2024-03-14 11:46:43 +0300

    Add subclass of Animal: Bird and Fish

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python3
-
 from parent_class import Animal
 
+
 class Bird(Animal):
-    pass
+    def __init__(self, animal_type, age, fly_height):
+        super().__init__(animal_type, age, weight=0)
+        self.__fly_height = fly_height
+
+    def get_speed(self):
+        return self.age * 5
+
+
+class Fish(Animal):
+    def __init__(self, animal_type, age, swim_deep):
+        super().__init__(animal_type, age, weight=0)
+        self.__swim_deep = swim_deep
+
+    def get_speed(self):
+        return self.age * 2
+
+    def print_animal(self):
+        print(f'Child method: {self}, I swim in river!')
+
+
+if __name__ == "__main__":
+    parrot = Bird("Bird", 5, 200)
+    gold_fish = Fish("Fish", 3, 100)
+
+    print(f'Parrot speed: {parrot.get_speed()}')
+    print(f'Gold fish speed: {gold_fish.get_speed()}')
+
+    parrot.print_animal()
+    gold_fish.print_animal()
